59d99cff34acb41ecf309e828f02330df7f9dc29
Angela <angeladimon91@gmail.com>
2021-10-26 15:50:14 -1000

    add get crypto file

---
+++ getCrypto.py
@@ -0,0 +1,8 @@
+import requests
+
+def getInfo(symbol):
+    response = requests.get("https://data.messari.io/api/v1/assets/"+symbol+"/metrics")
+    res = response.json()
+    name = res['data']['name']
+    price = res['data']['market_data']['price_usd']
+    return name, price
