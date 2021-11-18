b2a3aff7da791662d7e309709b0fb4ce417b8e75
Angela <angeladimon91@gmail.com>
2021-11-17 20:28:41 -1000

    remove commented out code

--- getCrypto.py
+++ getCrypto.py
@@ -7,12 +7,4 @@ def getInfo(symbol):
     res = response.json()
     name = res['data']['name']
     price = res['data']['market_data']['price_usd']
-
-    # today = date.today()
-    # startDate = today - relativedelta(days=-30)
-
-    # response = requests.get(f"https://data.messari.io/api/v1/assets/{symbol}/metrics/price/time-series?start={startDate}&end={today}&interval=1d&columns=close&format=json")
-    # print(response.json())
-
-
     return name, price
